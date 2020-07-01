a1bdae0ffc60ce4c204b7396b2bde0ed5ea27246
Julius-r <julius.robinson95@yahoo.com>
2020-06-30 21:51:56 -0500

    added email info

--- src/components/contact.js
+++ src/components/contact.js
@@ -6,7 +6,7 @@ export default function Contact() {
   <div class="container px-5 py-24 mx-auto">
     <div class="lg:w-2/3 flex flex-col sm:flex-row sm:items-center items-start mx-auto">
       <h1 class="flex-grow sm:pr-16 text-2xl font-medium title-font">I commend you on making it this far. You can either email me using the button below, or scroll further down and check out other places I'm at!</h1>
-      <a class="flex-shrink-0 border-0 py-2 px-8 rounded text-lg mt-10 sm:mt-0">Email Me!</a>
+      <a href="mailto:julius.robinson95@yahoo.com" class="flex-shrink-0 border-0 py-2 px-8 rounded text-lg mt-10 sm:mt-0">Email Me!</a>
     </div>
   </div>
 </section>
