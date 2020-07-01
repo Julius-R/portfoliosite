af63f5c86d637a79a3ae186af8b54f3fa780f558
Julius-r <julius.robinson95@yahoo.com>
2020-06-30 21:37:59 -0500

    changed config files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Julius Robinson Frontend Dev`,
+    description: `Hi! My name is Julius Robinson and I'm a Frontend Web Developer based in Dallas, Tx!`,
+    author: `Julius Robinson`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
