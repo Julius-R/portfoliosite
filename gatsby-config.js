7f2a72a1b14d04a738cd56a485065a7a289693c8
Julius-r <julius.robinson95@yahoo.com>
2020-07-09 00:33:04 -0500

    minor changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,14 +14,20 @@ module.exports = {
       },
     },
     {
-    resolve: `gatsby-plugin-sass`,
-    options: {
-      postCssPlugins: [
-        require("tailwindcss"),
-        require("./tailwind.config.js"), // Optional: Load custom Tailwind CSS configuration
-      ],
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-158107808-2",
+      },
     },
-  },
+    {
+      resolve: `gatsby-plugin-sass`,
+      options: {
+        postCssPlugins: [
+          require("tailwindcss"),
+          require("./tailwind.config.js"), // Optional: Load custom Tailwind CSS configuration
+        ],
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
